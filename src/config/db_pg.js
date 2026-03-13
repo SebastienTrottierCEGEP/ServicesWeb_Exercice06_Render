74ef3e73b149c7a35b916d3ffd6113f88d8f1a43
Sebastien Trottier <trottier.sebastien@cegepvicto.ca>
2026-03-13 17:37:33 -0400

    postgresSQL update

--- src/config/db_pg.js
+++ src/config/db_pg.js
@@ -4,13 +4,20 @@ import pg from 'pg'
 import dotenv from 'dotenv';
 dotenv.config();
 
-const pool = new pg.Pool({
+let params = { 
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  ssl: process.env.PG_SSL
-})
+  port: process.env.PG_PORT
+}
+
+// SSL est desactivé par default, mais on peut l'activer en mettant PG_SSL=true dans le .env
+// Nécessaire pour se connecter à la BD sur render.com de l'extérieur
+if (process.env.PG_SSL && process.env.PG_SSL.toLowerCase() === 'true') {
+  params.ssl = { rejectUnauthorized: false };
+}
+
+const pool = new pg.Pool(params);
 
 export default pool;
