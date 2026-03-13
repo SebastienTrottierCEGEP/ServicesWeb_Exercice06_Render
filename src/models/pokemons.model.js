9b49fac1eab0a6ac7004710719c68f5cab965791
Sebastien Trottier <trottier.sebastien@cegepvicto.ca>
2026-03-13 17:12:35 -0400

    postgresSQL update

--- src/models/pokemons.model.js
+++ src/models/pokemons.model.js
@@ -6,14 +6,22 @@ import pool from "../config/db_pg.js"; // PostgreSQL
 //  id: number
 export async function getPokemonForId(id) {
 
-    const requete = `SELECT id, nom, type_primaire, type_secondaire, pv, attaque, defense FROM pokemon WHERE id = $1;`;
+    const requete = `SELECT id,
+                            nom,
+                            type_primaire,
+                            type_secondaire,
+                            pv,
+                            attaque,
+                            defense 
+                    FROM public.pokemon
+                    WHERE id = $1;`;
     const parametres = [id];
     try {
         // .query() en PostgreSQL
         const resultats = await pool.query(requete, parametres);
         return resultats.rows[0] || null; // Retourne le premier résultat ou null si aucun résultat
     } catch (erreur) {
-        console.error(`Erreur getPokemonForId(), code: ${erreur.code} sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
+        console.error(`Erreur getPokemonForId(): ${erreur}`);
         throw erreur;
     }
 };
@@ -33,15 +41,22 @@ export function numPagesForTotalPokemons(nombreTotalPokemons) {
 export async function getPokemons(page) {
 
     const offset = (page - 1) * POKEMONS_PAR_PAGE;
-    const requete = `SELECT id, nom, type_primaire, type_secondaire, pv, attaque, defense 
-                    FROM pokemon LIMIT $1 OFFSET $2`;
+    const requete = `SELECT id,
+                            nom,
+                            type_primaire,
+                            type_secondaire,
+                            pv,
+                            attaque,
+                            defense 
+                    FROM public.pokemon
+                    LIMIT $1
+                    OFFSET $2`;
     const parametres = [POKEMONS_PAR_PAGE, offset];
     try {
         const resultats = await pool.query(requete, parametres);
         return resultats.rows;
     } catch (erreur) {
-        console.error(`Erreur getPokemons(), code: ${erreur.code} sqlState ${erreur.sqlState} : 
-                       ${erreur.sqlMessage}`);
+        console.error(`Erreur getPokemons(): ${erreur}`);
         throw erreur;
     }
 };
@@ -49,11 +64,10 @@ export async function getPokemons(page) {
 // returns: int
 export async function getTotalPokemons() {
     try {
-        const resultats = await pool.query(`SELECT COUNT(*) as total FROM pokemon;`);
+        const resultats = await pool.query(`SELECT COUNT(*) as total FROM public.pokemon;`);
         return resultats.rows[0].total;
     } catch (erreur) {
-        console.error(`Erreur getTotalPokemons(), code: ${erreur.code} sqlState ${erreur.sqlState} : 
-                       ${erreur.sqlMessage}`);
+        console.error(`Erreur getTotalPokemons(): ${erreur}`);
         throw erreur;
     }
 };
@@ -64,15 +78,21 @@ export async function getTotalPokemons() {
 export async function getPokemonsPourType(page, type) {
 
     const offset = (page - 1) * POKEMONS_PAR_PAGE;
-    let requete = `SELECT id, nom, type_primaire, type_secondaire, pv, attaque, defense 
-                    FROM pokemon WHERE type_primaire = $1 LIMIT $2 OFFSET $3`;
+    let requete = `SELECT id,
+                          nom,
+                          type_primaire,
+                          type_secondaire,
+                          pv,
+                          attaque,
+                          defense 
+                    FROM public.pokemon 
+                    WHERE type_primaire = $1 LIMIT $2 OFFSET $3`;
     const parametres = [type, POKEMONS_PAR_PAGE, offset];
     try {
         const resultats = await pool.query(requete, parametres);
         return resultats.rows;
     } catch (erreur) {
-        console.error(`Erreur getPokemons(), code: ${erreur.code} sqlState ${erreur.sqlState} : 
-                       ${erreur.sqlMessage}`);
+        console.error(`Erreur getPokemons(): ${erreur}`);
         throw erreur;
     }
 };
@@ -80,11 +100,10 @@ export async function getPokemonsPourType(page, type) {
 // returns: int
 export async function getTotalPokemonsPourType(type) {
     try {
-        const resultats = await pool.query(`SELECT COUNT(*) as total FROM pokemon WHERE type_primaire = $1;`, [type]);
+        const resultats = await pool.query(`SELECT COUNT(*) as total FROM public.pokemon WHERE type_primaire = $1;`, [type]);
         return resultats.rows[0].total;
     } catch (erreur) {
-        console.error(`Erreur getTotalPokemonsPourType(), code: ${erreur.code} sqlState ${erreur.sqlState} : 
-                       ${erreur.sqlMessage}`);
+        console.error(`Erreur getTotalPokemonsPourType(): ${erreur}`);
         throw erreur;
     }
 };
@@ -93,17 +112,14 @@ export async function getTotalPokemonsPourType(type) {
 export async function ajoutPokemon(nom, type_primaire, type_secondaire, pv, attaque, defense) {
     // Ajoute un nouveau pokemon à la liste
     // console.log(`Ajout du pokemon : nom=${nom}, type_primaire=${type_primaire}, type_secondaire=${type_secondaire}, pv=${pv}, attaque=${attaque}, defense=${defense}`);
-    const requete = `INSERT INTO pokemon (nom, type_primaire, type_secondaire, pv, attaque, defense) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;`;
+    const requete = `INSERT INTO public.pokemon (nom, type_primaire, type_secondaire, pv, attaque, defense) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;`;
     const valeurs = [nom, type_primaire, type_secondaire, pv, attaque, defense];
     try {
-        // .query ou .execute peuvent être utilisés
-        // .execute est préféré pour les requêtes avec des valeurs à insérer 
-        //   (protection contre les injections SQL)
         const resultats = await pool.query(requete, valeurs);
         // console.log(`Pokemon ajouté avec succès, ID: ${resultat.insertId}`);
         return resultats.rows[0].id;
     } catch (erreur) {
-        console.error(`Erreur ajoutPokemon(), code: ${erreur.code} sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
+        console.error(`Erreur ajoutPokemon(): ${erreur}`);
         throw erreur;
     }
 };
@@ -111,20 +127,23 @@ export async function ajoutPokemon(nom, type_primaire, type_secondaire, pv, atta
 // returns: pokemon modifié ou null si id non trouvé
 export async function modifiePokemon(id, nom, type_primaire, type_secondaire, pv, attaque, defense) {
 
-    // console.log(`Modification du pokemon : id=${id}, nom=${nom}, type_primaire=${type_primaire}, type_secondaire=${type_secondaire}, pv=${pv}, attaque=${attaque}, defense=${defense}`);
-    const requete = `UPDATE pokemon SET nom = ?, type_primaire = ?, type_secondaire = ?, 
-                     pv = ?, attaque = ?, defense = ? WHERE id = ?`;
+    const requete = `UPDATE public.pokemon SET nom = $1, 
+                                        type_primaire = $2,
+                                        type_secondaire = $3, 
+                                        pv = $4,
+                                        attaque = $5,
+                                        defense = $6 
+                    WHERE id = $7
+                    RETURNING *;`
     const valeurs = [nom, type_primaire, type_secondaire, pv, attaque, defense, id];
     try {
-        // .execute est préféré pour les requêtes avec des valeurs à insérer 
-        //   (protection contre les injections SQL)
-        const [resultat] = await pool.execute(requete, valeurs);
-        if (resultat.affectedRows === 0) {
+        const resultats = await pool.query(requete, valeurs);
+        if (resultats.rows.length === 0) {
             return null; // Aucun pokemon modifié, id non trouvé
         }
-        return resultat; // Pokemon modifié avec succès
+        return resultats.rows[0]; // Pokemon modifié avec succès
     } catch (erreur) {
-        console.error(`Erreur modifiePokemon(), code: ${erreur.code} sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
+        console.error(`Erreur modifiePokemon(): ${erreur}`);
         throw erreur;
     }
 };
@@ -132,15 +151,13 @@ export async function modifiePokemon(id, nom, type_primaire, type_secondaire, pv
 // returns: true si pokemon supprimé, false si id non trouvé
 export async function supprimePokemon(id) {
 
-    const requete = `DELETE FROM pokemon WHERE id = ?`;
+    const requete = `DELETE FROM public.pokemon WHERE id = $1`;
     const valeurs = [id];
     try {
-        // .execute est préféré pour les requêtes avec des valeurs à insérer 
-        //   (protection contre les injections SQL)
-        const [resultat] = await pool.execute(requete, valeurs);
-        return (resultat.affectedRows != 0);
+        const resultats = await pool.query(requete, valeurs);
+        return (resultats.rowCount != 0);
     } catch (erreur) {
-        console.error(`Erreur supprimePokemon(), code: ${erreur.code} sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
+        console.error(`Erreur supprimePokemon(): ${erreur}`);
         throw erreur;
     }
 }
